6e6e5441f0a55b333fd83ecaccc80e5e642681e7
sayed <sayed.sadatibrahim@gmail.com>
2023-07-21 16:59:29 -0700

    text added

--- src/test/java/selenium_lectures/Xpath.java
+++ src/test/java/selenium_lectures/Xpath.java
@@ -12,7 +12,7 @@ public class Xpath {
 
 	public static void main(String[] args) throws InterruptedException {
 
-// webdriver manager
+// webdriver manager is used to manage....
 		WebDriverManager.chromedriver().setup();
 		
 		//WebDriver driver = new ChromeDriver();
