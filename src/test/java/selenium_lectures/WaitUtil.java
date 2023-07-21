f9b3791bae1b9d3fb15e2b357a0cf95f579c5051
sayed <sayed.sadatibrahim@gmail.com>
2023-07-21 16:34:36 -0700

    f

---
+++ src/test/java/selenium_lectures/WaitUtil.java
@@ -0,0 +1,38 @@
+package selenium_lectures;
+
+import java.time.Duration;
+import java.util.NoSuchElementException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class WaitUtil {
+
+	public static WebDriver driver;
+
+	public static void implicitWait() {
+
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
+	}
+
+	public static void explicitWait(WebElement ele) {
+
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+
+		wait.until(ExpectedConditions.elementToBeClickable(ele));
+	}
+
+	public static void fluentWait(WebElement ele) {
+
+		Wait<WebDriver> fwait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(30))
+				.pollingEvery(Duration.ofSeconds(5)).ignoring(NoSuchElementException.class);
+
+		fwait.until(ExpectedConditions.elementToBeClickable(ele));
+	}
+
+}
